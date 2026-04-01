8dc3a64aecf63cbe41d96da591e54b471ecda658
Stefano Caronia <stefano.caronia@gmail.com>
2026-04-01 21:25:18 +0200

    Enable line breaks in markdown: single newline → <br> in all formats

--- src/support/markdown.ts
+++ src/support/markdown.ts
@@ -7,6 +7,9 @@ import { marked } from "marked";
 import markedFootnote from "marked-footnote";
 
 // Configure marked once with all extensions
+marked.use({
+    breaks: true, // single newline → <br> (essential for creative writing)
+});
 marked.use(markedFootnote());
 
 export { marked };
